060181fae8a302de0411a784b712d4cefa53671d
Eric Wittmann <eric.wittmann@gmail.com>
2016-11-01 11:24:11 -0400

    new build of a-studio

--- studio/version.js
+++ studio/version.js
@@ -1,5 +1,5 @@
 var ApimanStudioInfo = {
-    version: "0.0.1",
-    builtOn: "Fri, Oct 28, 2016  1:50:35 PM",
+    version: "0.0.1 Build 7",
+    builtOn: "Tue, Nov  1, 2016 11:09:14 AM",
     url: "http://studio.apiman.io/"
 };
